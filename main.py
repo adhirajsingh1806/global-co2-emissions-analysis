85c9e37446b051c1103f5471481a801e975eb87e
Adhiraj Singh <75022578+adhirajsingh1806@users.noreply.github.com>
2026-04-20 23:31:45 +0530

    Change CSV data source to Google Drive link
    Updated data source URL for reading CSV file and ensured data processing steps remain intact.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 #pre-processing data
-df = pd.read_csv(r"C:\Users\adhir\OneDrive\Documents\Data Analytics Project\GCB2022v27_MtCO2_flat.csv", index_col = False)
+df = pd.read_csv(r"https://drive.google.com/uc?export=download&id=1gbTwzt4VkzrscLeBBCns3HcxhhjJ2b22", index_col = False)
 
 #removing duplicates and filtering the data for years greater than or equal to 2000 and for the top 10 countries in the world by population
 df = df.drop_duplicates()
